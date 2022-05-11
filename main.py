d6d4cd3f46a06c4869262e06d75a6721dfed1428
emmapass <69655469+emmapass@users.noreply.github.com>
2022-05-11 14:50:39 -0700

    filter add

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ data set columns: 'tweet_id', 'user_id', 'tweet_timestamp', 'keyword',
 To access data in a column: data.colname (ex. data.tweet_id)
 '''
 data = pd.read_csv('small_dataset.csv')
-print(data.columns)
+data_no_neutral = data[data.emotion.ne('no specific emotion') & data.sentiment.ne('neutral or mixed')] #the and is redundant
+data_no_wuhan = data_no_neutral[data_no_neutral.keyword.ne('corona') & data_no_neutral.keyword.ne('wuhan')]
+#data_no_neutral = data[]
+print(data_no_wuhan.keyword)
+#print(data_no_neutral.emotion)
