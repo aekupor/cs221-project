518a7ba074455982f8167dfcb664fef16ac12ea9
emmapass <69655469+emmapass@users.noreply.github.com>
2022-05-15 21:09:25 -0700

    sequentially preprocess data

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt #incase you guys want to do graphs in python
+#sources
+#https://towardsdatascience.com/%EF%B8%8F-load-the-same-csv-file-10x-times-faster-and-with-10x-less-memory-%EF%B8%8F-e93b485086c7
+
 '''
 data set columns: 'tweet_id', 'user_id', 'tweet_timestamp', 'keyword',
        'valence_intensity', 'fear_intensity', 'anger_intensity',
        'happiness_intensity', 'sadness_intensity', 'sentiment', 'emotion'
 To access data in a column: data.colname (ex. data.tweet_id)
 '''
-data = pd.read_csv('small_dataset.csv')
-data_no_neutral = data[data.emotion.ne('no specific emotion') & data.sentiment.ne('neutral or mixed')] #the and is redundant
-data_no_wuhan = data_no_neutral[data_no_neutral.keyword.ne('corona') & data_no_neutral.keyword.ne('wuhan')]
-#data_no_neutral = data[]
-print(data_no_wuhan.keyword)
-#print(data_no_neutral.emotion)
+chunk_list = []
+req_cols = ['tweet_timestamp', 'keyword',
+       'valence_intensity', 'fear_intensity', 'anger_intensity',
+       'happiness_intensity', 'sadness_intensity', 'sentiment', 'emotion']
+tp = pd.read_csv("tweetid_userid_keyword_sentiments_emotions_United States.csv", chunksize=50000,\
+                 dtype = {'valence_intensity': 'float16', 'fear_intensity': 'float16', \
+                          'anger_intensity': 'float16', 'happiness_intensity': 'float16', 'sadness_intensity': 'float16'},\
+                 usecols=req_cols) #remove tweet and user id
+for chunk in tp:
+    chunk_filtered = chunk[chunk.emotion.ne('no specific emotion')] #remove neutral data
+    chunk_list.append(chunk_filtered)
+    #print(chunk_filtered)
+df = pd.concat(chunk_list, ignore_index=True)
